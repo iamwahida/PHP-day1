f5482cfab8c7c086ee26dcc5b224c5da409f3d6a
Ericka Angeles <ericka.angelesgarcia@gmail.com>
2022-11-07 14:05:05 +0100

    added 1 and 2 exercise

--- ericka/index.php
+++ ericka/index.php
@@ -1,14 +1,32 @@
-$name="ericka";
+<?php 
+$name="Juana";
+$age="23 years old";
+$job="Manager";
+$sentence="My Name is ". "$name" . " ". "I am " . "$age ". "and work as a ". "$job.";
+$names=["Mark", "John", "George", "Lisa"];
+$new_array=array(
+    "cartoon"=>"Super Girls",
+    "anime"=>"Sailor Moon",
+    "character"=>"Mickey Mouse"
+);
+// we added a new  element to the array
+$new_array["website"]="www";
+print_r($new_array);
+echo $new_array["cartoon"];
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>My name is : <?=$name ?></h1>
-    <h2>Ericka</h2>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+    <body>
+    <h1> <?=$sentence?></h1>
+    <p class="team"> The third player in the team is <?=$names[2] ?></p>
+ 
 </body>
 </html>
